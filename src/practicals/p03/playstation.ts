54e7cab0f5e2186e1f0a9ec3af49cd5659e5bcbc
paekung <supakit.3t@gmail.com>
2026-02-16 09:42:48 +0700

    p03

--- src/practicals/p03/playstation.ts
+++ src/practicals/p03/playstation.ts
@@ -2,4 +2,9 @@ import { Product } from './product'
 
 export class Playstation extends Product {
   // TODO: implement class properties, constructor with super(...), and methods
+
+
+  constructor(name:string,generation:number,price:number) {
+    super(name)
+  }
 }
--- src/practicals/p03/product.ts
+++ src/practicals/p03/product.ts
@@ -1,2 +1,18 @@
-export class Product {}
+export class Product {
+    generation:number = 0;
+    price: number = 0
+    readonly DISCOUNT_PERCENT: number = 5
+
+    constructor(public name: string, public salary: number) {}
+
+    getPrice() : number {
+        return this.price
+    }
+
+    setPrice(price:number) : number {
+        return this.price = price
+    }
+
+
+}
 
