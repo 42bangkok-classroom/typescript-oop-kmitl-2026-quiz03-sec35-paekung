b36e4a096ffb742c4c57773482931d756e5f8caa
paekung <supakit.3t@gmail.com>
2026-02-16 09:19:00 +0700

    p01

--- src/practicals/p01/student.ts
+++ src/practicals/p01/student.ts
@@ -1,8 +1,8 @@
 export class Student {
-    firstname: string;
-    lastname: string;
+    firstname: string ="";
+    lastname: string= "";
 
-    printName(): string {
-        return `${this.firstname} (${this.lastname})`
+    printName() {
+        console.log(`${this.firstname} (${this.lastname})`)
     }
 }
