0004c91366c58a3f3c3a7173061d1698eee41b33
paekung <supakit.3t@gmail.com>
2026-02-16 09:09:03 +0700

    p01

--- src/practicals/p01/student.ts
+++ src/practicals/p01/student.ts
@@ -1,1 +1,8 @@
-export class Student {}
+export class Student {
+    firstname: string = ''
+    lastname: string = ''
+
+    printName(): string {
+        return this.firstname + this.lastname
+    }
+}
