5fd74ba30f524bcfb324e490ec5454c3246cfb59
paekung <supakit.3t@gmail.com>
2026-02-16 09:29:22 +0700

    p02

--- src/practicals/p02/person.ts
+++ src/practicals/p02/person.ts
@@ -2,8 +2,18 @@ export class Person {
     firstname: string = ''
     lastname: string = ''
     private age: number = 0
+    static COUNTRY: string = 'Thailand'
 
-    setAge() : number {
-        this.age = 
+    getFullName() : string {
+        return this.firstname + this.lastname
     }
+
+    setAge(age: number) : number {
+        return this.age = age
+    }
+
+    getAge(): number {
+        return this.age
+    }
+
 }
